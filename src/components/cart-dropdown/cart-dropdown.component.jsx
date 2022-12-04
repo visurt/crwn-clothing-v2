5ba3d970bdbd47012c1ad4515ee56c3b4b17c331
visurt <visurt.dev@gmail.com>
2022-12-04 14:42:08 +0700

    added addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
+
 import Button from '../button/button.component';
+
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-item' />
+            <div className='cart-item'>
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} CartItem={item} />
+                ))}
+            </div>
             <Button>CHECKOUT</Button>
         </div>
     );
